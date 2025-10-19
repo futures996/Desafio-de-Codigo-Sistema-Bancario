628daf52d2cf44a8c9bd52e7a66e7eea5e63790a
Luan Felix <prpluan@gmail.com>
2025-10-19 08:59:47 -0300

    Update auth.py

--- routes/models/src/pages/models/routes/routes/routes/routes/src/pages/src/pages/auth.py
+++ routes/models/src/pages/models/routes/routes/routes/routes/src/pages/src/pages/auth.py
@@ -1,17 +1,21 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 SECRET_KEY = "sua-chave-secreta"
 ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def criar_token(dados: dict):
+    dados_copia = dados.copy()
+    expira = datetime.utcnow() + timedelta(minutes=30)
+    dados_copia.update({"exp": expira})
+    return jwt.encode(dados_copia, SECRET_KEY, algorithm=ALGORITHM)
 
 def verificar_token(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        cpf = payload.get("sub")
-        if cpf is None:
-            raise HTTPException(status_code=401, detail="Token inválido")
-        return cpf
+        return payload.get("sub")
     except JWTError:
         raise HTTPException(status_code=401, detail="Token inválido")
